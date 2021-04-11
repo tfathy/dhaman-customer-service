0f22d3f1e41390fda4994a96ba04987f5aac658c
TarekBakr <tarek@dhaman.org>
2021-04-11 09:01:20 +0300

    finshe application status modal

--- src/app/application-status/app-status-detail/app-status-detail.component.ts
+++ src/app/application-status/app-status-detail/app-status-detail.component.ts
@@ -10,9 +10,31 @@ export class AppStatusDetailComponent implements OnInit {
 @Input() applicationNo: number;
 @Input() cntDescription: string;
 @Input() applicationDate: Date;
+@Input() countRisksCovered: number;
+@Input() importerNameE: string;
+@Input() importerCountryE: string;
+@Input() applicationCurrency: string;
+@Input() applicationAmt: number;
+@Input() applValueUsd: number;
+@Input() decisionAmount: number;
+@Input()decisionCurrency: string;
+@Input() creditPeriod: number;
+@Input()term: string;
+@Input()currentStatus: number;
+@Input()ocSpecialConditions: string;
+
+statusDesc: string;
   constructor(private modalCtrl: ModalController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if( this.currentStatus ===1){
+      this.statusDesc = 'In process';
+    }else if(this.currentStatus === 3){
+      this.statusDesc = 'Rejected';
+    }else{
+      this.statusDesc = 'Accepted';
+    }
+  }
 
   onDismiss(){
     this.modalCtrl.dismiss();
